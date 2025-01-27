6db43e71ac7a5af686d034b1c52ee42e13edd005
Your Name <you@example.com>
2025-01-27 21:50:55 +0500

    update

--- labs/lab1/3.py
+++ labs/lab1/3.py
@@ -1,2 +1,9 @@
-x = 5 
-y = "Hello,World!"
+#This is a comment
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
--- labs/lab1/6.py
+++ labs/lab1/6.py
@@ -1,3 +1,48 @@
-x = str(3) # x will be '3'
-y = int(3) # y will be 3
-z = float(3) #z will be 3.0
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
+
+x = "awesome"
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
+
